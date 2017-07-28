45ed5e764edf26b4e5f50b6f20ea9552ea30a27c
alexfdz <alexfdz@gmail.com>
2017-07-28 20:43:03 +0100

    #8 reviewed by peterthompson in pull request #109

--- scripts/server_with_routes.js
+++ scripts/server_with_routes.js
@@ -27,6 +27,6 @@ const server = http.createServer(function (req, res) {
     }
 });
 
-server.listen(5000);     
-
-console.log('Node.js web server at port 5000 is running..');
+server.listen(5000, () => {
+  console.log('Node.js web server at port 5000 is running...');
+});
--- scripts/simple_server.js
+++ scripts/simple_server.js
@@ -4,6 +4,6 @@ const server = http.createServer(function (req, res) {
     res.end("Hello World!")
 });
 
-server.listen(5000);
-
-console.log('Node.js web server at port 5000 is running..');
+server.listen(5000, () => {
+  console.log('Node.js web server at port 5000 is running...');
+});
